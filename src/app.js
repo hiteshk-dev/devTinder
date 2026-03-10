9f41dbdb75c5afba26b9c544ce584be9595b1b50
Hitesh Kumar <hiteshkumar00099@gmail..com>
2026-03-11 00:16:48 +0530

    regex

--- src/app.js
+++ src/app.js
@@ -2,37 +2,34 @@ const express = require('express')
 
 const app = express(); //initalizer
 
-app.use("/hello/2",(req,res)=>{ //write nested one first
-    res.send("Hello from 2")
-})
-
-app.use("/hello",(req,res)=>{ 
-    res.send('Hello Hello Hello')
-})
-
-// app.use('/user',(req,res)=>{
-//     res.send("HAHAHAHAHAHA")
-// })
-//Only get call
-app.get("/user",(req,res)=>{
+// ab, abc
+app.get("/ab?c",(req,res)=>{
     res.send({firstName:'Akshay',lastName:'Saini'})
+})
+
+// abc, abbbbbbbc
+app.get("/ab+c",(req,res)=>{
+    res.send("Hello")
+})
+
+// abAKSHAYcd
+app.get("/ab*cd",(req,res)=>{
+    res.send("Hellp")
+})
+
+// ad, abcd
+app.get("/a(bc)?d",(req,res)=>{
+    res.send("Hello")
+})
+
+// works if path contains a
+app.get(/a/,(req,res)=>{
+    res.send("if a then works")
 })
 
 app.post("/user",(req,res)=>{
     res.send("Data saved successfully to the database")
 })
 
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Successfully")  
-})
-
-//this will match all the HTTP method API calls to /test
-app.use("/test",(req,res)=>{ //request handlers
-    res.send('Hello from the Server!')
-})
-
-app.use("/",(req,res)=>{
-    res.send('namaste from the dashboard!')
-})
 
 app.listen(3000) //listener
